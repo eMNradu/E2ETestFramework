69d9436fa20fa2945607c165f3c9bb64617505a1
Piraten <Piraten@192.168.0.192>
2018-10-22 22:07:16 +0300

    created automation project

---
+++ E2ESeleniumJenkinsSetUp/src/testcases/TC002.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC002 {
+	
+	@Test
+		public void testcas1() {
+			System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+			ChromeDriver driver = new ChromeDriver();
+			driver.get("http://www.facebook.com");
+			driver.findElementById("email").sendKeys("Hello");
+			driver.findElementById("pass").sendKeys("Hello");
+			driver.quit();
+		}
+
+
+}
+
